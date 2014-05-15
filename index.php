870b49e7e51852e53a686e4860f68e8a046a1774
eyalkd <eyalkd@hotmail.com>
2014-05-15 17:15:16 +0300

    Moved google signin functionality to separate folder.

--- index.php
+++ index.php
@@ -14,104 +14,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-include_once "templates/base.php";
 session_start();
 
 set_include_path("src/" . PATH_SEPARATOR . get_include_path());
 require_once 'Google/Client.php';
 require_once 'Google/Service/Urlshortener.php';
 require_once 'Google/Service/Plus.php';
-
-/************************************************
-  ATTENTION: Fill in these values! Make sure
-  the redirect URI is to this page, e.g:
-  http://localhost:8080/user-example.php
- ************************************************/
- $client_id = '929183822302-1uci18s700c7sq3062gg85vopep2ione.apps.googleusercontent.com';
- $client_secret = 'T54a4Vc2W58INeJ8g8Z1r76s';
- $redirect_uri = 'http://localhost/~etayschur/GIT_SPACE/TsamidDBFinal/TsamidDBFinal/index.php';
-
-/************************************************
-  Make an API request on behalf of a user. In
-  this case we need to have a valid OAuth 2.0
-  token for the user, so we need to send them
-  through a login flow. To do this we need some
-  information from our API console project.
- ************************************************/
-$client = new Google_Client();
-$client->setClientId($client_id);
-$client->setClientSecret($client_secret);
-$client->setRedirectUri($redirect_uri);
-
-// add the wanted services to request
-$client->addScope("https://www.googleapis.com/auth/userinfo.profile");
-$client->addScope("https://www.googleapis.com/auth/userinfo.email");
-$plus = new Google_Service_Plus($client);
-
-
-
-/************************************************
-  When we create the service here, we pass the
-  client to it. The client then queries the service
-  for the required scopes, and uses that when
-  generating the authentication URL later.
- ************************************************/
-$service = new Google_Service_Urlshortener($client);
-
-/************************************************
-  If we're logging out we just need to clear our
-  local access token in this case
- ************************************************/
-if (isset($_REQUEST['logout'])) {
-  unset($_SESSION['access_token']);
-}
-
-/************************************************
-  If we have a code back from the OAuth 2.0 flow,
-  we need to exchange that with the authenticate()
-  function. We store the resultant access token
-  bundle in the session, and redirect to ourself.
- ************************************************/
-if (isset($_GET['code'])) {
-  $client->authenticate($_GET['code']);
-  $_SESSION['access_token'] = $client->getAccessToken();
-  $redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
-  header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
-}
-
-/************************************************
-  If we have an access token, we can make
-  requests, else we generate an authentication URL.
- ************************************************/
-if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
-  $client->setAccessToken($_SESSION['access_token']);
-} else {
-  //$google_token= json_decode($_SESSION['access_token']);
-  //$client->refreshToken($google_token->refresh_token);
-  //$_SESSION['access_token']= $client->getAccessToken();
-  $authUrl = $client->createAuthUrl();
-}
-
-/************************************************
-  If we're signed in and have a request to shorten
-  a URL, then we create a new URL object, set the
-  unshortened URL, and call the 'insert' method on
-  the 'url' resource. Note that we re-store the
-  access_token bundle, just in case anything
-  changed during the request - the main thing that
-  might happen here is the access token itself is
-  refreshed if the application has offline access.
- ************************************************/
-if ($client->getAccessToken() && isset($_GET['url'])) {
-  $url = new Google_Service_Urlshortener_Url();
-  $url->longUrl = $_GET['url'];
-  $short = $service->url->insert($url);
-  $_SESSION['access_token'] = $client->getAccessToken();
-}
+require_once 'php/google-signin.php'
 
 ?>
-
-
 
 <!doctype html>
 <html lang="en" ng-app="myApp" ng-controller="MainController">
@@ -147,10 +58,10 @@ if ($client->getAccessToken() && isset($_GET['url'])) {
     <?php endif ?>
   </div>
 </div>
-  <div> <? 
+  <div> <?php
    if (isset($_SESSION['access_token'])) {
     $me = $plus->people->get("me");
-      ?> 
+    ?>
         <div class="wrapper">
             <div class="header col-md-16">
            <!--  <h1 class="col-md-8 col-md-offset-4" id="admin_title"> Tsamid Admin App </h1> -->
@@ -170,11 +81,11 @@ if ($client->getAccessToken() && isset($_GET['url'])) {
       <div class="alert_section col-md-14 col-md-offset-1"> <div>
       </div>
     </div>
-      <?
+      <?php
 
     }else{
       ?> <div> Please Log In </div>
-           <?
+           <?php
     }
     ?>
   </div>
--- php/google-signin.php
+++ php/google-signin.php
@@ -1,7 +1,96 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Eyalkd
- * Date: 15/05/14
- * Time: 17:05
- */ 
+
+/************************************************
+ATTENTION: Fill in these values! Make sure
+the redirect URI is to this page, e.g:
+http://localhost:8080/user-example.php
+ ************************************************/
+$client_id = '929183822302-1uci18s700c7sq3062gg85vopep2ione.apps.googleusercontent.com';
+$client_secret = 'T54a4Vc2W58INeJ8g8Z1r76s';
+$redirect_uri = 'http://localhost/sites/TsamidDBFinal/index.php';
+
+/************************************************
+Make an API request on behalf of a user. In
+this case we need to have a valid OAuth 2.0
+token for the user, so we need to send them
+through a login flow. To do this we need some
+information from our API console project.
+ ************************************************/
+$client = new Google_Client();
+$client->setClientId($client_id);
+$client->setClientSecret($client_secret);
+$client->setRedirectUri($redirect_uri);
+
+// add the wanted services to request
+$client->addScope("https://www.googleapis.com/auth/userinfo.profile");
+$client->addScope("https://www.googleapis.com/auth/userinfo.email");
+$plus = new Google_Service_Plus($client);
+
+
+
+/************************************************
+When we create the service here, we pass the
+client to it. The client then queries the service
+for the required scopes, and uses that when
+generating the authentication URL later.
+ ************************************************/
+$service = new Google_Service_Urlshortener($client);
+
+/************************************************
+If we're logging out we just need to clear our
+local access token in this case
+ ************************************************/
+if (isset($_REQUEST['logout'])) {
+    unset($_SESSION['access_token']);
+}
+
+/************************************************
+If we have a code back from the OAuth 2.0 flow,
+we need to exchange that with the authenticate()
+function. We store the resultant access token
+bundle in the session, and redirect to ourself.
+ ************************************************/
+if (isset($_GET['code'])) {
+    $auth = $client->authenticate($_GET['code']);
+    $_SESSION['access_token'] = $client->getAccessToken();
+    $redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+    header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
+    $token = $_SESSION['access_token'];
+    $refresh_token = $token['refresh_token'];
+    $client->refreshToken($refresh_token);
+
+}
+
+/************************************************
+If we have an access token, we can make
+requests, else we generate an authentication URL.
+ ************************************************/
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+    $client->setAccessToken($_SESSION['access_token']);
+} else {
+    //$google_token= json_decode($_SESSION['access_token']);
+    //$client->refreshToken($google_token->refresh_token);
+    //$_SESSION['access_token']= $client->getAccessToken();
+    $authUrl = $client->createAuthUrl();
+}
+
+/************************************************
+If we're signed in and have a request to shorten
+a URL, then we create a new URL object, set the
+unshortened URL, and call the 'insert' method on
+the 'url' resource. Note that we re-store the
+access_token bundle, just in case anything
+changed during the request - the main thing that
+might happen here is the access token itself is
+refreshed if the application has offline access.
+ ************************************************/
+if ($client->getAccessToken() && isset($_GET['url'])) {
+    $url = new Google_Service_Urlshortener_Url();
+    $url->longUrl = $_GET['url'];
+    $short = $service->url->insert($url);
+    $_SESSION['access_token'] = $client->getAccessToken();
+
+}
+
+?>
+
