0a710d26a8c1a0e4b0b439b254b831a7373292f5
EtaySchur <EtaySchur@gmail.com>
2014-05-24 15:38:41 +0300

    fix

--- static/js/google_handler.js
+++ static/js/google_handler.js
@@ -9,11 +9,11 @@ var currentUser;
 
 (function() {
 
-    var po = document.createElement('script');
-    po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/client:plusone.js?onload=render';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(po, s);
+    //var po = document.createElement('script');
+    //po.type = 'text/javascript'; po.async = true;
+    //po.src = 'https://apis.google.com/js/client:plusone.js?onload=render';
+    //var s = document.getElementsByTagName('script')[0];
+    //s.parentNode.insertBefore(po, s);
 })();
 
 /* Executed when the APIs finish loading */
@@ -21,23 +21,23 @@ function render() {
 
     // Additional params including the callback, the rest of the params will
     // come from the page-level configuration.
-    var additionalParams = {
-        'callback': signinCallback
-    };
+    //var additionalParams = {
+    //    'callback': signinCallback
+    //};
 
-    gapi.auth.signIn(additionalParams); // Will use page level configuration
+//    gapi.auth.signIn(additionalParams); // Will use page level configuration
 
 }
 
 function signinCallback(authResult) {
-    if (authResult['status']['signed_in']) {
-        console.log("ENERING RESULT!!!!");
-        currentUser = authResult;
-        console.log("Current User " , currentUser );
+    //if (authResult['status']['signed_in']) {
+      //  console.log("ENERING RESULT!!!!");
+        //currentUser = authResult;
+        //console.log("Current User " , currentUser );
         // Update the app to reflect a signed in user
         // Hide the sign-in button now that the user is authorized, for example:
         //onsole.log('result',authResult);
-        gapi.client.load('plus','v1', function(){
+        //gapi.client.load('plus','v1', function(){
 
             //var request = gapi.client.plus.people.search({
             //    'query' : 'etay schur',
@@ -47,18 +47,18 @@ function signinCallback(authResult) {
             //request.execute(function(resp) {
             //    //console.log(resp);
             //});
-        });
+        //});
 
 
-    } else {
+  //  } else {
         // Update the app to reflect a signed out user
         // Possible error values:
         //   "user_signed_out" - User is signed-out
         //   "access_denied" - User denied access to your app
         //   "immediate_failed" - Could not automatically log in the user
         console.log('Sign-in state: ' + authResult['error']);
-    }
-}
+   // }
+}s
 
 
 
