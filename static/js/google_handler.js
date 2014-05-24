0cd8030ebfaaf423717e4114387fb108749df089
EtaySchur <EtaySchur@gmail.com>
2014-05-24 16:11:00 +0300

    console

--- static/js/google_handler.js
+++ static/js/google_handler.js
@@ -18,37 +18,7 @@ var currentUser;
 
 
 
-function signinCallback(authResult) {
-    console.log('CALLBACK GOOGLE HANDLER');
-    if (authResult['status']['signed_in']) {
-        console.log("ENERING RESULT!!!!");
-        currentUser = authResult;
-        console.log("Current User " , currentUser );
-        // Update the app to reflect a signed in user
-        // Hide the sign-in button now that the user is authorized, for example:
-        //onsole.log('result',authResult);
-        gapi.client.load('plus','v1', function(){
 
-            //var request = gapi.client.plus.people.search({
-            //    'query' : 'etay schur',
-            //    'maxResults' : 5
-            //});
-
-            //request.execute(function(resp) {
-            //    //console.log(resp);
-            //});
-        });
-
-
-    } else {
-        // Update the app to reflect a signed out user
-        // Possible error values:
-        //   "user_signed_out" - User is signed-out
-        //   "access_denied" - User denied access to your app
-        //   "immediate_failed" - Could not automatically log in the user
-        console.log('Sign-in state: ' + authResult['error']);
-    }
-}
 
 
 
--- static/js/parse_admin.js
+++ static/js/parse_admin.js
@@ -29,6 +29,39 @@ function render() {
 }
 
 
+function signinCallback(authResult) {
+    console.log('CALLBACK GOOGLE Parse admin');
+    if (authResult['status']['signed_in']) {
+        console.log("ENERING RESULT!!!!");
+        currentUser = authResult;
+        console.log("Current User " , currentUser );
+        // Update the app to reflect a signed in user
+        // Hide the sign-in button now that the user is authorized, for example:
+        //onsole.log('result',authResult);
+        gapi.client.load('plus','v1', function(){
+
+            //var request = gapi.client.plus.people.search({
+            //    'query' : 'etay schur',
+            //    'maxResults' : 5
+            //});
+
+            //request.execute(function(resp) {
+            //    //console.log(resp);
+            //});
+        });
+
+
+    } else {
+        // Update the app to reflect a signed out user
+        // Possible error values:
+        //   "user_signed_out" - User is signed-out
+        //   "access_denied" - User denied access to your app
+        //   "immediate_failed" - Could not automatically log in the user
+        console.log('Sign-in state: ' + authResult['error']);
+    }
+}
+
+
 ParseManager.prototype.googlePlusSignin = function (callback){
     console.log('google sign in start..');
     var po = document.createElement('script');
