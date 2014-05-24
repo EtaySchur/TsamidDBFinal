9474923dc73ee727955bd063de5cd940b0de5d1c
eyalkd <eyalkd@hotmail.com>
2014-05-24 17:21:12 +0300

    remove old functions for getting favorites.

--- static/js/parse.js
+++ static/js/parse.js
@@ -7,10 +7,6 @@ var LOCAL_FOOD_PATH = "assets/images/myZonePage/";
 var LOCAL_BADGE_PATH = "assets/images/badges/";
 var LOCAL_AVATAR_PATH = "assets/images/avatarImages/";
 var LOCAL_FULL_AVATAR_PATH = "assets/images/fullAvatarImages/";
-var LOCAL_MUSIC_PATH = "assets/images/myZonePage/";
-var LOCAL_MOVIES_PATH = "assets/images/myZonePage/";
-var LOCAL_ANIMALS_PATH = "assets/images/myZonePage/";
-var LOCAL_HOBBIES_PATH = "assets/images/myZonePage/";
 var LOCAL_FAVORITES_PATH = "assets/images/myZonePage/";
 
 /**
@@ -149,6 +145,10 @@ function addFavoriteToUser(callback, favoriteId, user)
     });
 }
 
+/**
+ * get array of arrays with user favorites.
+ * ex: [food: Array[2], music: Array[3]]
+ */
 function getUserFavorites(callback, user)
 {
     var counter = 0;
@@ -179,105 +179,6 @@ function getUserFavorites(callback, user)
     }
 }
 
-/**
-* Adding the user new favorite food
-*/
-function addFavoriteFoodToUser (food, user)
-{
-    var favoriteFoodArray = new Array();
-    favoriteFoodArray = user.getFavoriteFood();
-    favoriteFoodArray.push(food); // This will add the favorite food to the local user object
-
-    Parse.User.current().set("favoriteFood", favoriteFoodArray, null);
-    Parse.User.current().save().then(
-        function(food) {
-            console.log('Favorite food was added');
-        },
-        function(error) {
-            console.log('Favorite food was not added, with error code: ' + error.description);
-        }
-    );
-}
-
-/**
- * Adding the user new favorite music
- */
-function addFavoriteMusicToUser (music, user)
-{
-    var musicArray = new Array();
-    musicArray = user.getFavoriteMusic();
-    musicArray .push(music); // This will add the favorite music to the local user object
-
-    Parse.User.current().set("favoriteMusic", musicArray, null);
-    Parse.User.current().save().then(
-        function(music) {
-            console.log('Favorite music was added');
-        },
-        function(error) {
-            console.log('Favorite music was not added, with error code: ' + error.description);
-        }
-    );
-}
-
-/**
- * Adding the user new favorite movies
- */
-function addFavoriteMoviesToUser (movies, user)
-{
-    var moviesArray = new Array();
-    moviesArray = user.getFavoriteMovies();
-    moviesArray .push(movies); // This will add the favorite movies to the local user object
-
-    Parse.User.current().set("favoriteMovies", moviesArray, null);
-    Parse.User.current().save().then(
-        function(movies) {
-            console.log('Favorite movies was added');
-        },
-        function(error) {
-            console.log('Favorite movies was not added, with error code: ' + error.description);
-        }
-    );
-}
-
-/**
- * Adding the user new favorite animals
- */
-function addFavoriteAnimalsToUser (animals, user)
-{
-    var animalsArray = new Array();
-    animalsArray = user.getFavoriteAnimals();
-    animalsArray .push(animals); // This will add the favorite animals to the local user object
-
-    Parse.User.current().set("favoriteAnimals", animalsArray, null);
-    Parse.User.current().save().then(
-        function(animals) {
-            console.log('Favorite animals was added');
-        },
-        function(error) {
-            console.log('Favorite animals was not added, with error code: ' + error.description);
-        }
-    );
-}
-
-/**
- * Adding the user new favorite hobbies
- */
-function addFavoriteHobbiesToUser (hobbies, user)
-{
-    var hobbiesArray = new Array();
-    hobbiesArray = user.getFavoriteHobbies();
-    hobbiesArray .push(hobbies); // This will add the favorite hobbies to the local user object
-
-    Parse.User.current().set("favoriteHobbies", hobbiesArray, null);
-    Parse.User.current().save().then(
-        function(hobbies) {
-            console.log('Favorite hobbies was added');
-        },
-        function(error) {
-            console.log('Favorite hobbies was not added, with error code: ' + error.description);
-        }
-    );
-}
 
 /**
 * Retriving the given user badges, in the callback function an associative array will be received
@@ -330,140 +231,6 @@ function getAllUserAchievements (callback, user) {
               }
           );
   });
-}
-
-/**
-* Retriving the given user favorite food, in the callback function an associative array will be received
-*/
-function getAllUserFavoriteFood (callback, user) {
-  var usersTable = Parse.Object.extend("_User");
-  var query = new Parse.Query(usersTable);
-
-  query.get(Parse.User.current().id).then(
-        function (parseUser) {
-          var foodTable = Parse.Object.extend("Food");
-          var query = new Parse.Query(foodTable);
-
-          query.containedIn("objectId", parseUser.get("favoriteFood"));
-          query.find().then(
-              function (results) {
-                var foodArray = new Array();
-                var path = GLOBAL_PREFIX + LOCAL_FOOD_PATH;
-
-                for (i in results)
-                  foodArray.push({ "id":results[i].id, "path": path + results[i].get("path") }); // Creating the associative array
-                callback(foodArray);
-              }
-          );
-  });
-}
-
-/**
- * Retrieving the given user favorite music, in the callback function an associative array will be received
- */
-function getAllUserFavoriteMusic (callback, user)
-{
-    var usersTable = Parse.Object.extend("_User");
-    var query = new Parse.Query(usersTable);
-
-    query.get(Parse.User.current().id).then(
-        function (parseUser) {
-            var musicTable = Parse.Object.extend("Music");
-            var query = new Parse.Query(musicTable);
-
-            query.containedIn("objectId", parseUser.get("favoriteMusic"));
-            query.find().then(
-                function (results) {
-                    var musicArray = new Array();
-                    var path = GLOBAL_PREFIX + LOCAL_MUSIC_PATH;
-
-                    for (i in results)
-                        musicArray.push({ "id":results[i].id, "path": path + results[i].get("path") }); // Creating the associative array
-                    callback(musicArray);
-                }
-            );
-        });
-}
-
-/**
- * Retrieving the given user favorite movies, in the callback function an associative array will be received
- */
-function getAllUserFavoriteMovies (callback, user)
-{
-    var usersTable = Parse.Object.extend("_User");
-    var query = new Parse.Query(usersTable);
-
-    query.get(Parse.User.current().id).then(
-        function (parseUser) {
-            var movieTable = Parse.Object.extend("Movie");
-            var query = new Parse.Query(movieTable);
-
-            query.containedIn("objectId", parseUser.get("favoriteMovies"));
-            query.find().then(
-                function (results) {
-                    var moviesArray = new Array();
-                    var path = GLOBAL_PREFIX + LOCAL_MOVIES_PATH;
-
-                    for (i in results)
-                        moviesArray.push({ "id":results[i].id, "path": path + results[i].get("path") }); // Creating the associative array
-                    callback(moviesArray);
-                }
-            );
-        });
-}
-
-/**
- * Retrieving the given user favorite animals, in the callback function an associative array will be received
- */
-function getAllUserFavoriteAnimals (callback, user)
-{
-    var usersTable = Parse.Object.extend("_User");
-    var query = new Parse.Query(usersTable);
-
-    query.get(Parse.User.current().id).then(
-        function (parseUser) {
-            var animalTable = Parse.Object.extend("Animal");
-            var query = new Parse.Query(animalTable);
-
-            query.containedIn("objectId", parseUser.get("favoriteAnimals"));
-            query.find().then(
-                function (results) {
-                    var animalsArray = new Array();
-                    var path = GLOBAL_PREFIX + LOCAL_ANIMALS_PATH;
-
-                    for (i in results)
-                        animalsArray.push({ "id":results[i].id, "path": path + results[i].get("path") }); // Creating the associative array
-                    callback(animalsArray);
-                }
-            );
-        });
-}
-
-/**
- * Retrieving the given user favorite hobbies, in the callback function an associative array will be received
- */
-function getAllUserFavoriteHobbies (callback, user)
-{
-    var usersTable = Parse.Object.extend("_User");
-    var query = new Parse.Query(usersTable);
-
-    query.get(Parse.User.current().id).then(
-        function (parseUser) {
-            var hobbyTable = Parse.Object.extend("Hobby");
-            var query = new Parse.Query(hobbyTable);
-
-            query.containedIn("objectId", parseUser.get("favoriteHobbies"));
-            query.find().then(
-                function (results) {
-                    var hobbiesArray = new Array();
-                    var path = GLOBAL_PREFIX + LOCAL_HOBBIES_PATH;
-
-                    for (i in results)
-                        hobbiesArray.push({ "id":results[i].id, "path": path + results[i].get("path") }); // Creating the associative array
-                    callback(hobbiesArray);
-                }
-            );
-        });
 }
 
 /**
@@ -652,8 +419,8 @@ function getAllItems (callback, tableName, option) {
       avatarPath = GLOBAL_PREFIX + LOCAL_BADGE_PATH;
       break;
 
-    case "Food":
-      avatarPath = GLOBAL_PREFIX + LOCAL_FOOD_PATH;
+    case "Favorites":
+      avatarPath = GLOBAL_PREFIX + LOCAL_FAVORITES_PATH;
       break;
 
     default:
@@ -676,7 +443,7 @@ function getAllItems (callback, tableName, option) {
           callback(items);
         },
         function(error) {
-          console.log('Failed to get badges, with error code: ' + error.code);
+          console.log('Failed to get arrays, with error code: ' + error.code);
         }
   );
 }
@@ -717,21 +484,17 @@ function getAllOnlineUsers (callback) {
 * Inner function for creating custom user object from the parse user object
 */
 function createUserFromParseUser (parseUser) {
-  var user = new User();
+    var user = new User();
 
-  user.setName( parseUser.get("username") );
-  user.setEmail( parseUser.get("email") );
-  user.setPrivileges( parseUser.get("privileges") );
-  user.setGender( parseUser.get("gender") );
-  user.setAvatar( parseUser.get("avatar") );
-  user.setBadges( parseUser.get("badges") );
-  user.setFavoriteFood( parseUser.get("favoriteFood") );
-    user.setFavoriteFood( parseUser.get("favoriteMusic") );
-    user.setFavoriteFood( parseUser.get("favoriteMovies") );
-    user.setFavoriteFood( parseUser.get("favoriteAnimals") );
-    user.setFavoriteFood( parseUser.get("favoriteHobbies") );
+    user.setName( parseUser.get("username") );
+    user.setEmail( parseUser.get("email") );
+    user.setPrivileges( parseUser.get("privileges") );
+    user.setGender( parseUser.get("gender") );
+    user.setAvatar( parseUser.get("avatar") );
+    user.setBadges( parseUser.get("badges") );
+    user.setFavorites( parseUser.get("favorites") );
 
-  return user;
+    return user;
 }
 
 /**
--- static/js/parse_old_functions.js
+++ static/js/parse_old_functions.js
@@ -1,8 +1,8 @@
 
-/*
-THE FUNCTIONS ON THIS FILE ARE DEPRECATED!!!
 
-DON'T USE THEM!!!
+//THE FUNCTIONS ON THIS FILE ARE DEPRECATED!!!
+
+//DON'T USE THEM!!!
 
 
 
@@ -34,6 +34,239 @@ function testFavorites()
     }
 }
 
+ /**
+ * Adding the user new favorite food
+ */
+function addFavoriteFoodToUser (food, user)
+{
+    var favoriteFoodArray = new Array();
+    favoriteFoodArray = user.getFavoriteFood();
+    favoriteFoodArray.push(food); // This will add the favorite food to the local user object
+
+    Parse.User.current().set("favoriteFood", favoriteFoodArray, null);
+    Parse.User.current().save().then(
+        function(food) {
+            console.log('Favorite food was added');
+        },
+        function(error) {
+            console.log('Favorite food was not added, with error code: ' + error.description);
+        }
+    );
+}
+
+/**
+ * Adding the user new favorite music
+ */
+function addFavoriteMusicToUser (music, user)
+{
+    var musicArray = new Array();
+    musicArray = user.getFavoriteMusic();
+    musicArray .push(music); // This will add the favorite music to the local user object
+
+    Parse.User.current().set("favoriteMusic", musicArray, null);
+    Parse.User.current().save().then(
+        function(music) {
+            console.log('Favorite music was added');
+        },
+        function(error) {
+            console.log('Favorite music was not added, with error code: ' + error.description);
+        }
+    );
+}
+
+/**
+ * Adding the user new favorite movies
+ */
+function addFavoriteMoviesToUser (movies, user)
+{
+    var moviesArray = new Array();
+    moviesArray = user.getFavoriteMovies();
+    moviesArray .push(movies); // This will add the favorite movies to the local user object
+
+    Parse.User.current().set("favoriteMovies", moviesArray, null);
+    Parse.User.current().save().then(
+        function(movies) {
+            console.log('Favorite movies was added');
+        },
+        function(error) {
+            console.log('Favorite movies was not added, with error code: ' + error.description);
+        }
+    );
+}
+
+/**
+ * Adding the user new favorite animals
+ */
+function addFavoriteAnimalsToUser (animals, user)
+{
+    var animalsArray = new Array();
+    animalsArray = user.getFavoriteAnimals();
+    animalsArray .push(animals); // This will add the favorite animals to the local user object
+
+    Parse.User.current().set("favoriteAnimals", animalsArray, null);
+    Parse.User.current().save().then(
+        function(animals) {
+            console.log('Favorite animals was added');
+        },
+        function(error) {
+            console.log('Favorite animals was not added, with error code: ' + error.description);
+        }
+    );
+}
+
+/**
+ * Adding the user new favorite hobbies
+ */
+function addFavoriteHobbiesToUser (hobbies, user)
+{
+    var hobbiesArray = new Array();
+    hobbiesArray = user.getFavoriteHobbies();
+    hobbiesArray .push(hobbies); // This will add the favorite hobbies to the local user object
+
+    Parse.User.current().set("favoriteHobbies", hobbiesArray, null);
+    Parse.User.current().save().then(
+        function(hobbies) {
+            console.log('Favorite hobbies was added');
+        },
+        function(error) {
+            console.log('Favorite hobbies was not added, with error code: ' + error.description);
+        }
+    );
+}
+
+/**
+ * Retriving the given user favorite food, in the callback function an associative array will be received
+ */
+function getAllUserFavoriteFood (callback, user) {
+    var usersTable = Parse.Object.extend("_User");
+    var query = new Parse.Query(usersTable);
+
+    query.get(Parse.User.current().id).then(
+        function (parseUser) {
+            var foodTable = Parse.Object.extend("Food");
+            var query = new Parse.Query(foodTable);
+
+            query.containedIn("objectId", parseUser.get("favoriteFood"));
+            query.find().then(
+                function (results) {
+                    var foodArray = new Array();
+                    var path = GLOBAL_PREFIX + LOCAL_FOOD_PATH;
+
+                    for (i in results)
+                        foodArray.push({ "id":results[i].id, "path": path + results[i].get("path") }); // Creating the associative array
+                    callback(foodArray);
+                }
+            );
+        });
+}
+
+/**
+ * Retrieving the given user favorite music, in the callback function an associative array will be received
+ */
+function getAllUserFavoriteMusic (callback, user)
+{
+    var usersTable = Parse.Object.extend("_User");
+    var query = new Parse.Query(usersTable);
+
+    query.get(Parse.User.current().id).then(
+        function (parseUser) {
+            var musicTable = Parse.Object.extend("Music");
+            var query = new Parse.Query(musicTable);
+
+            query.containedIn("objectId", parseUser.get("favoriteMusic"));
+            query.find().then(
+                function (results) {
+                    var musicArray = new Array();
+                    var path = GLOBAL_PREFIX + LOCAL_MUSIC_PATH;
+
+                    for (i in results)
+                        musicArray.push({ "id":results[i].id, "path": path + results[i].get("path") }); // Creating the associative array
+                    callback(musicArray);
+                }
+            );
+        });
+}
+
+/**
+ * Retrieving the given user favorite movies, in the callback function an associative array will be received
+ */
+function getAllUserFavoriteMovies (callback, user)
+{
+    var usersTable = Parse.Object.extend("_User");
+    var query = new Parse.Query(usersTable);
+
+    query.get(Parse.User.current().id).then(
+        function (parseUser) {
+            var movieTable = Parse.Object.extend("Movie");
+            var query = new Parse.Query(movieTable);
+
+            query.containedIn("objectId", parseUser.get("favoriteMovies"));
+            query.find().then(
+                function (results) {
+                    var moviesArray = new Array();
+                    var path = GLOBAL_PREFIX + LOCAL_MOVIES_PATH;
+
+                    for (i in results)
+                        moviesArray.push({ "id":results[i].id, "path": path + results[i].get("path") }); // Creating the associative array
+                    callback(moviesArray);
+                }
+            );
+        });
+}
+
+/**
+ * Retrieving the given user favorite animals, in the callback function an associative array will be received
+ */
+function getAllUserFavoriteAnimals (callback, user)
+{
+    var usersTable = Parse.Object.extend("_User");
+    var query = new Parse.Query(usersTable);
+
+    query.get(Parse.User.current().id).then(
+        function (parseUser) {
+            var animalTable = Parse.Object.extend("Animal");
+            var query = new Parse.Query(animalTable);
+
+            query.containedIn("objectId", parseUser.get("favoriteAnimals"));
+            query.find().then(
+                function (results) {
+                    var animalsArray = new Array();
+                    var path = GLOBAL_PREFIX + LOCAL_ANIMALS_PATH;
+
+                    for (i in results)
+                        animalsArray.push({ "id":results[i].id, "path": path + results[i].get("path") }); // Creating the associative array
+                    callback(animalsArray);
+                }
+            );
+        });
+}
+
+/**
+ * Retrieving the given user favorite hobbies, in the callback function an associative array will be received
+ */
+function getAllUserFavoriteHobbies (callback, user)
+{
+    var usersTable = Parse.Object.extend("_User");
+    var query = new Parse.Query(usersTable);
+
+    query.get(Parse.User.current().id).then(
+        function (parseUser) {
+            var hobbyTable = Parse.Object.extend("Hobby");
+            var query = new Parse.Query(hobbyTable);
+
+            query.containedIn("objectId", parseUser.get("favoriteHobbies"));
+            query.find().then(
+                function (results) {
+                    var hobbiesArray = new Array();
+                    var path = GLOBAL_PREFIX + LOCAL_HOBBIES_PATH;
+
+                    for (i in results)
+                        hobbiesArray.push({ "id":results[i].id, "path": path + results[i].get("path") }); // Creating the associative array
+                    callback(hobbiesArray);
+                }
+            );
+        });
+}
 
 
 
@@ -43,6 +276,3 @@ function testFavorites()
 
 
 
-
-
-*/
